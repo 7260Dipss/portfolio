08ebf04bbc6d5881655fdef215cdcc148856f6f3
7260Dipss <dipalibangar68@gmail.com>
2025-04-16 15:44:37 +0530

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var util = require("util");
 var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"ganpati_bappa"
+    host:"bsjppxkejuxujek0lhzt-mysql.services.clever-cloud.com",
+    user:"u6haj7dxr8rxzxgy",
+    password:"NYBFYstnAk6Z0mG7z65K",
+    database:"bsjppxkejuxujek0lhzt"
 })
 var exe = util.promisify(conn.query).bind(conn);
 
